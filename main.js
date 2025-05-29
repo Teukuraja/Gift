c8c3c36cfe0b15a8b04304bc56f3ce152e7dde1a
Teuku Raja <teuku1923@gmail.com>
2025-05-29 16:29:51 +0800

    fix: tambah tag body agar popup muncul di HP

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-onload = () =>{
-        document.body.classList.remove("container");
-};
+window.onload = () => {
+  document.body.classList.remove("container");
 
-const flowers = document.querySelectorAll('.flower');
-const popup = document.getElementById('popupMessage');
+  const flowers = document.querySelectorAll('.flower');
+  const popup = document.getElementById('popupMessage');
 
-flowers.forEach(flower => {
-  flower.addEventListener('click', () => {
-    popup.classList.add('show');
-    setTimeout(() => {
-      popup.classList.remove('show');
-    }, 2500);
+  flowers.forEach(flower => {
+    flower.addEventListener('click', () => {
+      popup.classList.add('show');
+      setTimeout(() => {
+        popup.classList.remove('show');
+      }, 2500);
+    });
   });
-});
+};
